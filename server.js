44fef695577ec3526aa1078f8b228e66b3b9899f
Gerardo Barcia <Personal@mbp-de-gerardo.home>
2020-03-15 11:52:01 +0100

    adding socket io

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+var io = require('socket.io').listen(server);
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
